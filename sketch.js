ac4a9efe96846bfa134f80f0d69ad3c45f03fc5f
mariam05050 <mariamdiab0505@gmail.com>
2026-09-08 13:14:30 +0300

    Added 2D farm with primitives, animated windmill, plants, fence, hay bales, and rendering of the 13 seeded shapes using palette index

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+// SCENEFORGE - MAIN PROGRAM
+
 function setup() {
-  createCanvas(600, 400);
 
-  background(200);
+  // Start with a 2-D canvas.
+  // We will switch the project to WEBGL when Module 4
+  // is implemented.
+  createCanvas(1100, 700);
 
-  console.log("sketch.js loaded");
+  // Generate the 13 shapes from my ID seed.
+  generateScene();
+
+  // Print the generated data to the console.
+  printSceneData();
+}
+
+function draw() {
+
+  // Module 1:
+  // draw the complete farm every frame.
+  drawFarmScene();
 }
